a26ecbbd003eac74a80b56ecb59e5afdba2a73ff
YuanJie <email@example.com>
2025-08-01 14:15:52 +0800

    Update

--- xiaohashu-user/xiaohashu-user-biz/src/main/java/com/quanxiaoha/xiaohashu/user/biz/XiaohashuUserBizApplication.java
+++ xiaohashu-user/xiaohashu-user-biz/src/main/java/com/quanxiaoha/xiaohashu/user/biz/XiaohashuUserBizApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class APP  {
+public class XiaohashuUserBizApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(APP.class, args);
+        SpringApplication.run(com.quanxiaoha.xiaohashu.user.biz.XiaohashuUserBizApplication.class, args);
     }
+
 }
